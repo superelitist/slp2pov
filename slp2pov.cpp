6167490bc52f34eab161deed20387d0b8e6945d8
superelitist <superelitist@gmail.com>
2014-10-15 12:41:01 -0400

    added class triangle

--- slp2pov.cpp
+++ slp2pov.cpp
@@ -4,8 +4,25 @@
 #include "stdafx.h"
 using namespace std;
 
+
+class triangle {
+	float x_coord, y_coord, z_coord;
+	float x_normal, y_normal, z_normal;
+public:
+	void set_coords(float, float, float);
+	float coords() { return x_coord, y_coord, z_coord; }
+};
+
+void triangle::set_coords(float x, float y, float z) {
+	x_coord = x;
+	y_coord = y;
+	z_coord = z;
+}
+
+
 int _tmain(int argc, _TCHAR* argv[])
 {
+	cout << "Version 0.02\n";
 	if (argc != 2) // argc should be 2 for correct execution
 		cout << "usage: " << "slp2pov" << " <filename>\n"; // Original author recommended argv[0], but that outputs gibberish. I'm hardcoding this in lieu of a workaround.
 	else {
@@ -15,11 +32,11 @@ int _tmain(int argc, _TCHAR* argv[])
 		if (!the_file.is_open())
 			cout << "Could not open file\n";
 		else {
-			char x;
-			// the_file.get ( x ) returns false if the end of the file
-			//  is reached or an error occurs
-			while (the_file.get(x))
-				cout << x;
+			string line;
+			while (the_file.good()){ // good() returns true except for an eof or other error.
+				getline (the_file , line);
+				cout << line << endl;
+			}
 		}
 		// the_file is closed implicitly here
 	}
