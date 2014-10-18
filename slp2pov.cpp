77f3b46779cab5fb79c8af54e0a84dd96eeb70ad
superelitist <superelitist@gmail.com>
2014-10-17 21:43:21 -0400

    created class normal, moved test to isolated function.

--- slp2pov.cpp
+++ slp2pov.cpp
@@ -16,6 +16,17 @@ public:
 	float * get_coords() { return coords; }
 };
 
+class normal {
+	float coords[3]; // ALWAYS x y z
+public:
+	void set_coords(float x, float y, float z) {
+		coords[0] = x;
+		coords[1] = y;
+		coords[2] = z;
+	}
+	float * get_coords() { return coords; }
+};
+
 class facet {
 	vertex points[3]; // ALWAYS a b c
 public:
@@ -26,7 +37,6 @@ public:
 	}
 	vertex * get_points() { return points; }
 	vertex get_a() { return points[0]; }
-//	vertex get_a() { return points[0]; }
 };
 
 // TODO: There should be a normal class very similar to the vertex class.
@@ -49,26 +59,7 @@ int _tmain(int argc, _TCHAR* argv[])
 				cout << line << endl;
 			}
 		// insert some code testing...
-			vertex v_a;
-			v_a.set_coords(1, 2, 3);
-			float *a = v_a.get_coords();
-			cout << "vertex v_a" << "=" << &v_a << endl;
-			cout << "x is " << a[0] << ", y is " << a[1] << " and z is " << a[2] << endl;
-			vertex v_b;
-			v_b.set_coords(2, 4, 6);
-			float *b = v_b.get_coords();
-			cout << "vertex v_b" << "=" << &v_b << endl;
-			cout << "x is " << b[0] << ", y is " << b[1] << " and z is " << b[2] << endl; 
-			vertex v_c;
-			v_c.set_coords(3, 6, 9);
-			float *c = v_c.get_coords();
-			cout << "vertex v_c" << "=" << &v_c << endl;
-			cout << "x is " << c[0] << ", y is " << c[1] << " and z is " << c[2] << endl;
-			facet f_01;
-			f_01.set_points(v_a, v_b, v_c);
-			float *f_01_v_a = f_01.get_points()[0].get_coords();
-			cout << "facet f_01" << &f_01 << endl;
-			cout << "a is at" << f_01_v_a[0] << endl;
+			
 		}
 		// the_file is closed implicitly here
 	}
@@ -83,3 +74,34 @@ TODO:
 + Define a class for colors, and a class for triangles.
 + Parse the input into an object for each color and triangle, ordered appropriately.
 */
+
+void test()
+{
+	vertex v_a;
+	v_a.set_coords(1, 2, 3);
+	float *a = v_a.get_coords();
+	cout << "vertex v_a => x is " << a[0] << ", y is " << a[1] << " and z is " << a[2] << endl;
+	vertex v_b;
+	v_b.set_coords(2, 4, 6);
+	float *b = v_b.get_coords();
+	cout << "vertex v_b => x is " << b[0] << ", y is " << b[1] << " and z is " << b[2] << endl;
+	vertex v_c;
+	v_c.set_coords(3, 6, 9);
+	float *c = v_c.get_coords();
+	cout << "vertex v_c => x is " << c[0] << ", y is " << c[1] << " and z is " << c[2] << endl;
+	facet f_01;
+	f_01.set_points(v_a, v_b, v_c);
+	float f01ax = f_01.get_points()[0].get_coords()[0];
+	float f01ay = f_01.get_points()[0].get_coords()[1];
+	float f01az = f_01.get_points()[0].get_coords()[2];
+	float f01bx = f_01.get_points()[1].get_coords()[0];
+	float f01by = f_01.get_points()[1].get_coords()[1];
+	float f01bz = f_01.get_points()[1].get_coords()[2];
+	float f01cx = f_01.get_points()[2].get_coords()[0];
+	float f01cy = f_01.get_points()[2].get_coords()[1];
+	float f01cz = f_01.get_points()[2].get_coords()[2];
+	cout << endl << "facet f_01:" << endl;
+	cout << "a is at => " << f01ax << ", " << f01ay << ", " << f01az << endl;
+	cout << "b is at => " << f01bx << ", " << f01by << ", " << f01bz << endl;
+	cout << "c is at => " << f01cx << ", " << f01cy << ", " << f01cz << endl;
+}
